acbc7b97650edf2536023566f3dcf9de644542a6
LavaSquids <79622877+LavaSquids@users.noreply.github.com>
2022-05-19 18:12:51 +0100

    Fix `Header` by changing to `Heading`

--- components/NewCategoryModel.jsx
+++ components/NewCategoryModel.jsx
@@ -5,12 +5,12 @@ import { getModule } from '@vizality/webpack';
 import { forceUpdatePrivateChannelsList, DefaultSettings, Class } from '../constants';
 
 const Modal = getModule(m => m.ModalRoot);
-const Header = getModule(m => m.displayName === 'Header' && m.Sizes);
+const { Heading } = getModule(m => m.Heading?.displayName === 'Heading');
 const FormItem = getModule(m => m.displayName === 'FormItem');
 const TextInput = getModule(m => m.displayName === 'TextInput');
 const A = getModule(m => m.BorderColors);
 
-const { header, modalTitle, closeButton, modalContent, marginBottom20 } = Class.header;
+const { header, closeButton, modalContent, marginBottom20 } = Class.header;
 const { alignCenter, vertical } = getModule(m => m.alignCenter && m.vertical);
 
 export default memo(({ transitionState, onClose, settingsGet, settingsSet, channelId }) => {
@@ -31,7 +31,7 @@ export default memo(({ transitionState, onClose, settingsGet, settingsSet, chann
 
   return <Modal.ModalRoot transitionState={transitionState} size={Modal.ModalSize.SMALL} aria-label={'DM Create Category Modal'}>
     <Modal.ModalHeader className={header} align={alignCenter} direction={vertical} separator={false}>
-      <Header className={modalTitle} size={Header.Sizes.SIZE_24} tag={Header.Tags.H2}>{'Create Category'}</Header>
+      <Heading variant={'heading-lg/medium'}>{'Create Category'}</Heading>
       <Modal.ModalCloseButton className={closeButton} onClick={onClose} />
     </Modal.ModalHeader>
     <Modal.ModalContent className={modalContent}>
